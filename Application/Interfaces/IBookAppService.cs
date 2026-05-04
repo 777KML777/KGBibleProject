220bb9f9f92bf66d221cac1d0bf35230350a947d
Kleber777 <klebermirandalima@gmail.com>
2026-05-04 13:59:28 -0300

    Estruturando com regions a cada de aplicação.

--- Application/Interfaces/IBookAppService.cs
+++ Application/Interfaces/IBookAppService.cs
@@ -2,9 +2,14 @@ namespace Application.Interfaces;
 
 public interface IBookAppService
 {
+    #region "CRUD Operations"
     public BookDto Create(BookInputModel input);
     public List<BookDto> Read();
-    public Task<BookDto> GetById(GetByIdInput input);
     public BookDto Update(int identifier, BookInputModel dto);
     public bool Delete(int identifier);
+    #endregion
+
+    #region "RCO - Region Commom Operation"
+    public Task<BookDto> GetById(GetByIdInput input);
+    #endregion
 }
