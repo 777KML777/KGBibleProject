c841b8c979d9ae85f6f377ba9ef2180742c90856
Kleber777 <klebermirandalima@gmail.com>
2026-05-06 11:55:06 -0300

    Implementando Include no GetById do repositório.

--- Domain/Entities/BookEntity.cs
+++ Domain/Entities/BookEntity.cs
@@ -2,19 +2,24 @@ namespace Domain.Entities;
 
 public class BookEntity : Tracker
 {
-
-    public BookEntity(){ }
+    public BookEntity() { }
     public BookEntity(string name, string testament, string description)
     {
         CreatedAt = DateTime.Now;
         Name = name;
         Description = description;
         Testament = testament;
-
     }
     public string Name { get; private set; }
     public string Testament { get; private set; }
     public string Description { get; private set; }
 
-    public int CharacterId {get; private set; }
+    #region "RELATIONAL PROPERTIES
+    public int CharacterId { get; private set; }
+    public CharacterEntity Author { get; private set; }
+    public void LinkAuthor(CharacterEntity author) => Author = author;
+    #endregion
+
+
+    // TODO: E se quisermos exibir todos os personagens do livro? Como iriamos diferenciar dos autores? 
 }
